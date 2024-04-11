f406f89515a9ca2d04437111b7dec4219270f016
Dawid Radzimski <dawid.radzimski@gmail.com>
2024-04-11 12:46:32 +0200

    hw12_updated files

--- src/database/models.py
+++ src/database/models.py
@@ -1,6 +1,8 @@
-from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy import Column, Integer, String, func
+from sqlalchemy.sql.sqltypes import Date, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.sql.sqltypes import Date
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import ForeignKey
 
 Base = declarative_base()
 
@@ -15,3 +17,16 @@ class Contact(Base):
     phone_number = Column(String(15), nullable=False)
     date_of_birth = Column(Date, nullable=False)
     additional_data = Column(String(750), nullable=False)
+    user_id = Column('user_id', ForeignKey('users.id', ondelete='CASCADE'), default=None)
+    user = relationship('User', backref="tags")
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(150), nullable=False, unique=True)
+    email = Column(String(250), nullable=False)
+    password = Column(String(255), nullable=False)
+    created_at = Column('crated_at', DateTime, default=func.now())
+    avatar = Column(String(255), nullable=True)
+    refresh_token = Column(String(255), nullable=True)
--- src/routes/contacts.py
+++ src/routes/contacts.py
@@ -1,14 +1,11 @@
 from typing import List
-
 from fastapi import APIRouter, HTTPException, Depends, status
 from sqlalchemy.orm import Session
-
 from src.database.db import get_db
 from src.schemas import ContactIn, ContactOut, ContactUpdate, ContactDelete
 from src.repository import contacts as repository_contacts
 
 router = APIRouter(prefix='/contacts')
-
 
 @router.get("/", response_model=List[ContactOut])
 async def read_contacts(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
